dce557fd68a11cf19389fb13df1b77dbf619afe3
Deshon, Ryan <ryanproject@gmail.com>
2019-10-14 16:54:59 -0400

    Added youtube

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -1,11 +1,30 @@
 <template>
     <div>
-        Poop!
+        <SearchBar @termChange="onTermChange"></SearchBar>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
+    import SearchBar from './components/SearchBar';
+    const API_KEY = 'AIzaSyDxuMetoOiYzRc8k9OMPRPzFansc-Olrmo';
+
     export default {
-        name: 'App'
+        name: 'App',
+        components: {
+            SearchBar
+        },
+        methods: {
+            onTermChange(searchTerm) {
+                axios.get('https://www.googleapis.com/youtube/v3/search', {
+                    params: {
+                        key: API_KEY,
+                        type: 'video',
+                        part: 'snippet',
+                        q: searchTerm
+                    }
+                }).then(response => console.log(response));
+            }
+        },
     };
 </script>
